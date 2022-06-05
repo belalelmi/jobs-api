677f4ae50a64d698f034af1766d30f48acfdab14
belalelmi <belalelmi@gmail.com>
2022-06-05 18:13:10 -0400

    added security packages to app (xss, helmet)

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import "express-async-errors";
-// import path from 'path'
 import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
@@ -10,12 +9,18 @@ import connectDB from "./db/connect.js";
 // routers
 import authRouter from "./routes/auth.js";
 import jobsRouter from "./routes/jobs.js";
-// error handler
+// error handlers
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import { authentication } from "./middleware/authentication.js";
 
 dotenv.config();
+
+// extra security packages to protect the app
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import rateLimit from "express-rate-limit";
 
 connectDB();
 const app = express();
@@ -24,8 +29,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
