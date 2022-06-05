6d449961ad4642f07de6d737e9315568c5629252
belalelmi <belalelmi@gmail.com>
2022-06-04 23:17:49 -0400

    adding authentication middleware for jobs routes

---
+++ middleware/authentication.js
@@ -0,0 +1,26 @@
+import User from "../models/User.js";
+import JWT from "jsonwebtoken";
+import ayncHandler from "express-async-handler";
+import UnauthenticatedError from "../errors/unauthenticated.js";
+
+const authentication = ayncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = JWT.verify(token, process.env.JWT_SECRET);
+
+    // console.log(payload);
+
+    req.user = { userId: payload.userID, name: payload.name };
+
+    next();
+  } catch (err) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+});
+
+export { authentication };
